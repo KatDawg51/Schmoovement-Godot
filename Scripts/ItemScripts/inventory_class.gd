a70470d1511b3798a72be2265846783508fc9ae3
KatDawg51 <krystianszkubel@gmail.com>
2025-01-12 12:46:28 -0500

    Getting ready to make a post about dis

--- Scripts/ItemScripts/inventory_class.gd
+++
@@ -1,9 +0,0 @@
-class_name Inventory extends Node3D
-
-#Current_Gun
-@export var current_item:PackedScene:
-	set(new_item):
-		for child in get_children():
-			child.reparent(get_tree().root, true)
-		current_item = new_item
-		add_child(current_item.instantiate())
--- Scripts/PlayerScripts/player_class.gd
+++ Scripts/PlayerScripts/player_class.gd
@@ -1,25 +1,25 @@
 class_name Player extends CharacterBody3D
 
-#Refrences
+##Refrences
 @export_group("Refrences")
 @export var head:Node3D
 @export var cam:Camera3D
 @export var look_cast:RayCast3D
-@export var inventory:Inventory
 @export var cam_rig:Node3D
 @export var spring_arm:SpringArm3D
 @export var ground_normal_ray:RayCast3D
 @export var vault_cast:ShapeCast3D
 @export var collision:CollisionShape3D
 @export var anim_plr:AnimationPlayer
-
-
+#Timers
 @onready var jump_buff:SceneTreeTimer
 @onready var coyote_timer:SceneTreeTimer
 @onready var jump_debounce:SceneTreeTimer
 @onready var slide_time:SceneTreeTimer
+#Speed GUI
+@onready var speed_gui = globals.game.gui.get_child(0)
 
-#Stats
+##Stats
 @export_category("Speed")
 @export var walk_speed:float
 @export var sprint_speed:float
@@ -27,8 +27,6 @@ class_name Player extends CharacterBody3D
 @export var jump_power:float
 @export var jump_boost:float
 @export_category("Vault")
-#Vault Vars
-var vault_momentum:float
 @export var vault_height:float = 8
 @export var vault_boost:float = 6
 @export var vault_decay:float = 2
@@ -43,9 +41,6 @@ var vault_momentum:float
 @export var air_control:float = 30.0
 @export var air_speed:float = 15.0
 @export var fall_speed:float = 30
-#Sliding
-@export var slide_control:float
-@export var slide_speed:float
 @export var gravity:float = ProjectSettings.get_setting("physics/3d/default_gravity")
 @export_category("Timers")
 @export var jump_cool:float
@@ -60,39 +55,34 @@ var vault_momentum:float
 @export var base_FOV:= 80.0
 @export var FOV_change := 1.5
 
-#Refrences
-@onready var speed_gui = globals.game.gui.get_child(0)
-
-#Movement
+##Trackers
+#Velocity = Speed + Boost
 var speed:Vector3
 var boost:Vector3
+#Direct Input and Input Relative to Character
 var input_dir:Vector2
 var dir:Vector3
+#Relative Input with Slower Strafe Speed
 var ground_dir:Vector3
-var real_vel:Vector3
+#Allow Coyote Timer to Begin
 var coyote:bool = false
+#Clamped Velocity for Headbob and FOV
 var clamped_velocity:float
-#Cam Shake
-var current_rotation:Vector3
-var target_rotation:Vector3
-#Cam Shake Settings
-var snap:float
-var return_speed:float
-var shake_strength:Vector3
-#Bob Variables
+#Headbob Variables
 var bob_time:float = 0.0
 var smoothed_amp:float
 var bob_pos:Vector3
-#Slide Vars
-var can_slide_boost := true
+#Vault Boost Tracker
+var vault_momentum:float
 
-#States Enum
-enum states {walk, run, air, slide}
+#States Enum for "State Machine"
+enum states {ground, run, air}
 
 #Lock Mouse
-func _ready():
-	#Capture Mouse
+func _ready() -> void:
+	#Lock Mouse
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
 	#Timer Setup
 	jump_buff = get_tree().create_timer(0)
 	coyote_timer = get_tree().create_timer(0)
@@ -100,40 +90,32 @@ func _ready():
 	slide_time = get_tree().create_timer(0)
 
 #Inputs
-func _input(event):
+func _input(event) -> void:
 	#Quitting
 	if event.is_action_pressed("quit"):
 		get_tree().quit()
-	#Character Rotation
+
+	#Handle Character Rotation
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens))
 		head.rotate_x (deg_to_rad(-event.relative.y * sens))
 		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-89),deg_to_rad(89))
 
-#Movement
+#Runs Ever Physics Frame
 func _physics_process(delta:float) -> void:
-	#Get Direction
-	input_dir = Input.get_vector("left", "right", "up", "down").normalized()
-	dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
-	ground_dir = transform.basis * Vector3(input_dir.x if input_dir.y < 0 else input_dir.x * 0.8 , 0, input_dir.y if input_dir.y < 0 else input_dir.y * 0.8)
-	#Movement
-	update(delta)
-	#Jumping
-	up(delta)
-	#FOV
-	clamped_velocity = min(velocity.length(), sprint_speed*2)
-	var target_fov = base_FOV + FOV_change * clamped_velocity
-	cam.fov = lerp(cam.fov, target_fov, delta * 8.0)
-	#Headbob
+	#Run Functions
+	set_input_dirs()
+	FOV(delta)
 	headbob(delta)
+	update(delta)
+	up(delta)
 
 	#Update Speed GUI
-
+	speed_gui.text = str(round(Vector2(velocity.x, velocity.z).length()))
 
 #Jump Action
-func up(delta):
+func up(delta:float):
 	vault_momentum = move_toward(vault_momentum, 0, vault_decay * delta)
-	speed_gui.text = str(round(vault_momentum))
 	boost = dir * vault_momentum
 	#Detection
 	if Input.is_action_just_pressed("jump"):
@@ -175,14 +157,18 @@ func up(delta):
 				jump_buff = get_tree().create_timer(0)
 				jump_debounce = get_tree().create_timer(jump_cool)
 
+func set_input_dirs() -> void:
+	input_dir = Input.get_vector("left", "right", "up", "down").normalized()
+	dir = transform.basis * Vector3(input_dir.x, 0, input_dir.y)
+	ground_dir = transform.basis * Vector3(input_dir.x if input_dir.y < 0 else input_dir.x * 0.8 , 0, input_dir.y if input_dir.y < 0 else input_dir.y * 0.8)
 
-#Update Trimp Boost
-func trimp(delta):
-	var boosty:Vector3 = get_slope() * velocity.dot(get_slope()) * clamp(abs(velocity.y), 1, 10) / 100
-	speed += boosty
+func FOV(delta:float) -> void:
+	clamped_velocity = min(velocity.length(), sprint_speed*2)
+	var target_fov = base_FOV + FOV_change * clamped_velocity
+	cam.fov = lerp(cam.fov, target_fov, delta * 8.0)
 
 #Update Head Bob
-func headbob(delta) -> void:
+func headbob(delta:float) -> void:
 	#Smoothing The Sine Waves Position
 	var smoothed_pos:Vector3 = lerp(spring_arm.transform.origin, bob_pos * clamped_velocity * float(is_on_floor()) if dir else Vector3.ZERO, 0.01)
 	#Sine Wave
@@ -193,7 +179,7 @@ func headbob(delta) -> void:
 	bob_time += delta * velocity.length()
 	spring_arm.transform.origin = smoothed_pos
 
-#Returns The Slope
+#UNUSED: Returns Slope Using GroundNormalRay
 func get_slope() -> Vector3:
 	var normal:Vector3
 	if ground_normal_ray.is_colliding():
@@ -214,10 +200,7 @@ func coyote_timeout():
 func state() -> states:
 	#Ground Stats
 	if is_on_floor():
-		if Input.is_action_pressed("sprint"):
-			return states.slide
-		else:
-			return states.walk
+		return states.ground
 	else:
 		return states.air
 
@@ -232,8 +215,7 @@ func update(delta:float) -> void:
 	#Match States
 	match state():
 		#Walking
-		states.walk:
-			can_slide_boost = true
+		states.ground:
 			var moving = true if dir or not is_equal_approx(speed.length(), 0) or not dir.dot(get_real_velocity()) <= 0 else false
 			speed = speed.move_toward(ground_dir * walk_speed, (ground_acel if moving else ground_decel) * delta)
 			#Coyote Time Reset
@@ -243,31 +225,8 @@ func update(delta:float) -> void:
 				coyote = true
 		#Midair
 		states.air:
-			can_slide_boost = true
 			#Gravity
 			speed.y = move_toward(speed.y, -fall_speed, gravity * delta)
 			#Air Strafing
 			speed.x = move_toward(speed.x, dir.x * air_speed, air_control * delta)
 			speed.z = move_toward(speed.z, dir.z * air_speed, air_control * delta)
-		#Midair
-		states.slide:
-			if can_slide_boost:
-				speed *= 1.5
-				can_slide_boost = false
-			var cur_speed := speed.dot(dir)
-			var booster := clampf(slide_speed - cur_speed, 0, slide_control * delta)
-			speed = get_real_velocity() + booster * dir
-
-
-
-
-
-
-#func _process(delta:float) -> void:
-	#target_rotation = target_rotation.slerp(Vector3.ZERO, return_speed * delta)
-	#current_rotation = current_rotation.slerp(target_rotation, snap * delta)
-	##Set Camera Controllers Rotation
-	#cam_rig.rotation = current_rotation
-	##Fix Z Shake
-	#if shake_strength.z == 0:
-		#cam_rig.global_rotation.z = 0
